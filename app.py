e877e45ee7069d23ccb2955d47a3286756e43bd9
KevinKossi <mpungakevin@hotmail.com>
2023-08-26 17:29:35 +0200

    init repo

---
+++ app.py
@@ -0,0 +1,79 @@
+from flask import Flask, request , json
+import joblib
+import os
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+
+app = Flask(__name__)
+
+def checklabel(label):
+    mapping = {
+        0: "Bad",
+        1: "Mediocre",
+        2: "Good"
+    }
+    return mapping[label]
+
+@app.route('/Knn', methods=['POST'])
+def predict_knn():
+    # first load the model
+    dirname = os.getcwd()
+    filename_knn = os.path.join(dirname, 'models/knn')
+    knn = joblib.load(filename_knn)
+    # read the request data from the post request
+    response = request.get_json()
+    data = json.loads(response["data"])
+    # convert the data into a numpy array
+    arr = np.array(data)
+    # scale the data
+    scaler = StandardScaler()
+    arr = scaler.fit_transform(arr)
+    # predict the response
+    predictions = knn.predict(arr)
+    labels = map(checklabel, predictions.tolist())
+    labels = list(labels)
+    return json.dumps(labels)
+
+@app.route('/RandomForest', methods=['POST'])
+def predict_random_forest():
+     # first load the model
+    dirname = os.getcwd()
+    filename_randomforest = os.path.join(dirname, 'models/randomforest')
+    randomforest = joblib.load(filename_randomforest)
+    # read the request data from the post request
+    response = request.get_json()
+    data = json.loads(response["data"])
+    # convert the data into a numpy array
+    arr = np.array(data)
+    # scale the data
+    scaler = StandardScaler()
+    arr = scaler.fit_transform(arr)
+    # predict the response
+    predictions = randomforest.predict(arr)
+    labels = map(checklabel, predictions.tolist())
+    labels = list(labels)
+    return json.dumps(labels)
+
+@app.route('/DecisionTree', methods=['POST'])
+def predict_decision_tree():
+     # first load the model
+    dirname = os.getcwd()
+    filename_decisiontree = os.path.join(dirname, 'models/decisiontree')
+    decisiontree = joblib.load(filename_decisiontree)
+    # read the request data from the post request
+    response = request.get_json()
+    data = json.loads(response["data"])
+    # convert the data into a numpy array
+    arr = np.array(data)
+    # scale the data
+    scaler = StandardScaler()
+    arr = scaler.fit_transform(arr)
+    # predict the response
+    predictions = decisiontree.predict(arr)
+    labels = map(checklabel, predictions.tolist())
+    labels = list(labels)
+    return json.dumps(labels)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
